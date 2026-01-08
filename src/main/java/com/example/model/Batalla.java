4ca10f78569678ed30af47bcda951a0125e5c0af
a22anxoba <a22anxoba@iessanclemente.net>
2026-01-08 11:00:29 +0100

    Añadidos menores

---
+++ src/main/java/com/example/model/Batalla.java
@@ -0,0 +1,14 @@
+package com.example.model;
+
+import com.example.controlador.BosqueControlador;
+import com.example.controlador.DragonControlador;
+import com.example.controlador.HechizoControlador;
+import com.example.controlador.MagoControlador;
+import com.example.controlador.MonstruoControlador;
+import com.example.interfaz.Interfaz;
+
+public class Batalla {
+    
+
+    
+}
